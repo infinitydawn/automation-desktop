7393ac4b1b62850b65d2cb6ad2fb2d0bf0885aa9
W Huang <whuang4830@bths.edu>
2025-04-29 10:28:51 -0400

    update start.bat

--- Java/FX-400/start.bat
+++ Java/FX-400/start.bat
@@ -1,3 +1,3 @@
 python ./tools/excelReader.py
-javac -cp  dependencies/* FX400.java ZoneList.java Zone.java ReadTempArray.java DataEntryBot.java
-java -cp  dependencies/* FX400.java
+javac -cp  dependencies/* Automation.java FX400.java ZoneList.java Zone.java ReadTempArray.java DataEntryBot.java
+java -cp  dependencies/* Automation.java
