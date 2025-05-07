fd5ea659ab6aa8b5babef92ef674aa0c1a014ca3
W Huang <whuang4830@bths.edu>
2025-05-07 12:43:09 -0400

    update device classification

--- Java/FX-400/FX400.java
+++ Java/FX-400/FX400.java
@@ -47,7 +47,7 @@ public class FX400 extends Thread{
                 System.out.println(" - - - - -  + " + tags1[i]);
                 zoneList.addZone(Double.parseDouble(addresses[i]), tags1[i], tags2[i]);
 
-                if(Zone.checkTags(tags1[i], new String[] {"shutdown", "shut down"})){
+                if(Zone.checkTags(tags1[i], new String[] {"fan shut", "ac shut"})){
                     is_paused = true;
                 }
             }
