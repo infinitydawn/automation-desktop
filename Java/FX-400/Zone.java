179936fd416cae47e602c9e62462f4bf2d5dd9d6
W Huang <whuang4830@bths.edu>
2025-06-11 09:48:31 -0400

    update tags

--- Java/FX-400/Zone.java
+++ Java/FX-400/Zone.java
@@ -51,7 +51,7 @@ class Zone {
             type = "Latched Supervisory";
             this.isDualInput = true;
         } else if (Zone.checkTags(tag, new String[] { "valve", "tamper", "stat", "pump", "intake", "discharge",
-                "jockey", "jocky", "bypass", "radio trouble", "low heat", "generator", "dry sys", "elev pwr" })) {
+                "jockey", "jocky", "bypass", "radio trouble", "low heat", "generator", "dry sys", "elev pwr", "monitor" })) {
             type = "Non-latched Supervisory";
             this.isDualInput = true;
         } else if (Zone.checkTags(tag, new String[] { "smoke", "duct" })) {
@@ -76,7 +76,7 @@ class Zone {
         else if (Zone.checkTags(tag, new String[] { "phone mod" })) {
             type = "Telephone Module";
         }
-        else if (Zone.checkTags(tag, new String[] { "speakers" })) {
+        else if (Zone.checkTags(tag, new String[] { "speaker" })) {
             type = "Speakers";
         }
         else if (Zone.checkTags(tag, new String[] { "blank" , "spare" })) {
