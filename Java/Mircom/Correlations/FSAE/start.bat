af78b92b8fb3174e714fc610d8ff611de234baba
W Huang <whuang4830@bths.edu>
2025-11-27 10:47:03 -0500

    fix start

--- Java/Mircom/Correlations/FSAE/start.bat
+++ Java/Mircom/Correlations/FSAE/start.bat
@@ -1,3 +1,3 @@
-javac -cp  ../dependencies/* ../../DataEntryBot.java  FSAEBot.java Automation.java -d .
-java --enable-native-access=ALL-UNNAMED -cp "../dependencies/*;."  Automation.java
+javac -cp  ../../dependencies/* ../../DataEntryBot.java  FSAEBot.java Automation.java -d .
+java --enable-native-access=ALL-UNNAMED -cp "../../dependencies/*;."  Automation.java
 PAUSE
