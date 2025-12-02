5d9287864efd5f1780da4e1a6d439133f5621e39
W Huang <whuang4830@bths.edu>
2025-12-02 15:39:37 -0500

    dual input fix

--- Java/Mircom/Zone.java
+++ Java/Mircom/Zone.java
@@ -20,6 +20,7 @@ public class Zone {
     }
 
     public void addSubZone(double subAddress, String tag1, String tag2) {
+        this.isDualInput = true;
         this.subAddress = new subZone(subAddress, tag1, tag2);
     }
 
